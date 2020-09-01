d359b224a563e54a4ae6a9c7804d05dde21fed2d
pabmordel <pabmordel@alum.us.es>
2020-09-01 19:43:59 +0200

    Initial commit

---
+++ src/main/population/create-schema.sql
@@ -0,0 +1,173 @@
+
+    create table `administrator` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `anonymous` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `authenticated` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `challenge` (
+       `id` integer not null,
+        `version` integer not null,
+        `average` varchar(255),
+        `averagereward_amount` double precision,
+        `averagereward_currency` varchar(255),
+        `deadline` datetime(6),
+        `description` varchar(255),
+        `expert` varchar(255),
+        `expertreward_amount` double precision,
+        `expertreward_currency` varchar(255),
+        `rookie` varchar(255),
+        `rookiereward_amount` double precision,
+        `rookiereward_currency` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `consumer` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        `company` varchar(255),
+        `sector` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `inquiry` (
+       `id` integer not null,
+        `version` integer not null,
+        `creationdate` datetime(6),
+        `deadline` datetime(6),
+        `description` varchar(255),
+        `email` varchar(255),
+        `intervalmax_amount` double precision,
+        `intervalmax_currency` varchar(255),
+        `intervalmin_amount` double precision,
+        `intervalmin_currency` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `notice` (
+       `id` integer not null,
+        `version` integer not null,
+        `body` varchar(255),
+        `creationdate` datetime(6),
+        `deadline` datetime(6),
+        `header` varchar(255),
+        `links` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `overture` (
+       `id` integer not null,
+        `version` integer not null,
+        `creationdate` datetime(6),
+        `deadline` datetime(6),
+        `description` varchar(255),
+        `email` varchar(255),
+        `intervalmax_amount` double precision,
+        `intervalmax_currency` varchar(255),
+        `intervalmin_amount` double precision,
+        `intervalmin_currency` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `provider` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        `company` varchar(255),
+        `sector` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `technology_record` (
+       `id` integer not null,
+        `version` integer not null,
+        `description` varchar(255),
+        `email` varchar(255),
+        `inventor` varchar(255),
+        `sector` varchar(255),
+        `source` varchar(255),
+        `stars` integer,
+        `title` varchar(255),
+        `web` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `tool_record` (
+       `id` integer not null,
+        `version` integer not null,
+        `description` varchar(255),
+        `email` varchar(255),
+        `inventor` varchar(255),
+        `sector` varchar(255),
+        `source` varchar(255),
+        `stars` integer,
+        `title` varchar(255),
+        `web` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `user_account` (
+       `id` integer not null,
+        `version` integer not null,
+        `enabled` bit not null,
+        `identity_email` varchar(255),
+        `identity_name` varchar(255),
+        `identity_surname` varchar(255),
+        `password` varchar(255),
+        `username` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `hibernate_sequence` (
+       `next_val` bigint
+    ) engine=InnoDB;
+
+    insert into `hibernate_sequence` values ( 1 );
+
+    alter table `user_account` 
+       add constraint UK_castjbvpeeus0r8lbpehiu0e4 unique (`username`);
+
+    alter table `administrator` 
+       add constraint FK_2a5vcjo3stlfcwadosjfq49l1 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `anonymous` 
+       add constraint FK_6lnbc6fo3om54vugoh8icg78m 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `authenticated` 
+       add constraint FK_h52w0f3wjoi68b63wv9vwon57 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `consumer` 
+       add constraint FK_6cyha9f1wpj0dpbxrrjddrqed 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `provider` 
+       add constraint FK_b1gwnjqm6ggy9yuiqm0o4rlmd 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
